565be925ee96357cdce30c5fa31acb559bb73a82
Douglas Triggs <doug@opscode.com>
2014-02-20 16:43:00 -0700

    Use config parameters for create -- kinda sorta doesn't really work yet

--- lib/kitchen/driver/vagrant_metal.rb
+++ lib/kitchen/driver/vagrant_metal.rb
@@ -16,12 +16,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'chef/client'
-require 'chef_metal/provisioner'
-require 'chef/platform'
-require 'chef/providers'
-require 'chef/resources'
-require 'chef/resource/vagrant_cluster'
+#puts require 'chef/client'
+puts require 'chef/node'
+puts require 'chef/run_context'
+puts require 'chef/event_dispatch/dispatcher'
+# Already included, but including anyway:
+puts require 'chef/recipe'
+puts require 'chef/runner'
+
+# Replace all this with chef_metal require at some point:
+#puts require 'chef_metal/provisioner'
+#puts require 'chef/platform'
+puts require 'chef/providers'
+puts require 'chef/resources'
+puts require 'chef/resource/vagrant_cluster'
+#puts require 'chef/provider/vagrant_cluster'
 
 module Kitchen
   module Driver
@@ -41,24 +50,22 @@ module Kitchen
 #      default_config :vagrantfile_erb,
 #        File.join(File.dirname(__FILE__), "../../../templates/Vagrantfile.erb")
 
-#      default_config :provider,
-#        ENV.fetch('VAGRANT_DEFAULT_PROVIDER', "virtualbox")
+      default_config :provider,
+        ENV.fetch('VAGRANT_DEFAULT_PROVIDER', "virtualbox")
 
-# Probably need these eventually, uncomment at that point:
+      default_config :vm_hostname do |driver|
+        "#{driver.instance.name}.vagrantup.com"
+      end
 
-#      default_config :vm_hostname do |driver|
-#        "#{driver.instance.name}.vagrantup.com"
-#      end
+      default_config :box do |driver|
+        "opscode-#{driver.instance.platform.name}"
+      end
 
-#      default_config :box do |driver|
-#        "opscode-#{driver.instance.platform.name}"
-#      end
+      default_config :box_url do |driver|
+        driver.default_box_url
+      end
 
-#      default_config :box_url do |driver|
-#        driver.default_box_url
-#      end
-
-#      required_config :box
+      required_config :box
 
 #      no_parallel_for :create, :destroy
 
@@ -77,13 +84,17 @@ module Kitchen
 
       def create(state)
         # create_vagrantfile
-        # run_pre_create_command
+        run_pre_create_command
         # cmd = "vagrant up --no-provision"
         # cmd += " --provider=#{config[:provider]}" if config[:provider]
         # run cmd
         # set_ssh_state(state)
-        # info("Vagrant instance #{instance.to_str} created.")
         # TODO: stuff here
+        puts "--"
+        puts config
+        puts config[:provider]
+        info("Working on Vagrant instance #{instance.to_str}.")
+        puts "--"
         node = Chef::Node.new
         node.name 'test'
         node.automatic[:platform] = 'test'
@@ -92,6 +103,9 @@ module Kitchen
         run_context = Chef::RunContext.new(node, {},
           Chef::EventDispatch::Dispatcher.new(kitchen_sink))
         recipe = Chef::Recipe.new('test', 'test', run_context)
+        box = config[:box]
+        hostname = config[:vm_hostname]
+        box_url = config[:box_url]
         recipe.instance_eval do
           vagrant_cluster "#{ENV['HOME']}/machinetest"
 
@@ -99,15 +113,16 @@ module Kitchen
 
           with_chef_local_server :chef_repo_path => "#{ENV['HOME']}/machinetest/repo"
 
-          vagrant_box 'precise64' do
-            url 'http://files.vagrantup.com/precise64.box' 
+          vagrant_box box do
+            box_url
           end
 
-          machine 'test' do
+          machine hostname do
             action :create
           end
         end
         Chef::Runner.new(run_context).converge
+        info("Vagrant instance #{instance.to_str} created.")
       end
 
       def converge(state)
@@ -148,13 +163,13 @@ module Kitchen
 #        resolve_config!
 #      end
 
-#      def default_box_url
-#        bucket = config[:provider]
-#        bucket = 'vmware' if config[:provider] =~ /^vmware_(.+)$/
+      def default_box_url
+        bucket = config[:provider]
+        bucket = 'vmware' if config[:provider] =~ /^vmware_(.+)$/
 
-#        "https://opscode-vm-bento.s3.amazonaws.com/vagrant/#{bucket}/" +
-#          "opscode_#{instance.platform.name}_chef-provisionerless.box"
-#      end
+        "https://opscode-vm-bento.s3.amazonaws.com/vagrant/#{bucket}/" +
+          "opscode_#{instance.platform.name}_chef-provisionerless.box"
+      end
 
       protected
 
@@ -171,11 +186,11 @@ module Kitchen
 #          :live_stream => nil, :quiet => logger.debug? ? false : true)
 #      end
 
-#      def run_pre_create_command
-#        if config[:pre_create_command]
-#          run(config[:pre_create_command], :cwd => config[:kitchen_root])
-#        end
-#      end
+      def run_pre_create_command
+        if config[:pre_create_command]
+          run(config[:pre_create_command], :cwd => config[:kitchen_root])
+        end
+      end
 
 #      def vagrant_root
 #        @vagrant_root ||= File.join(
